e4464643f95bc5c834189554c62787771c5c7e7c
AviraLS22 <aviralsharma.sharma2205@gmail.com>
2024-12-27 10:15:19 +0530

    llm.ts comments

--- src/llm.ts
+++ src/llm.ts
@@ -1,25 +1,26 @@
-import { zodFunction } from 'openai/helpers/zod' 
-import { z } from 'zod'
-import type { AIMessage } from '../types'
-import { openai } from './ai'
-import { systemPrompt } from './systemPrompt'
+import { zodFunction } from 'openai/helpers/zod' // to validate input using zod schema
+import { z } from 'zod'// to define and validate schema
+import type { AIMessage } from '../types'// typescript type 
+import { openai } from './ai'// openai api
+import { systemPrompt } from './systemPrompt'// system prompt to define the protocols 
 
-export const runLLM = async ({
-  model = 'gpt-4o-mini',
-  messages,
-  temperature = 0.1,
-  tools,
+
+export const runLLM = async ({  // function to run the LLM model
+  model = 'gpt-4o-mini',            // specifies the model of the llm used 
+  messages,                         // an array of AImessages representing chat history 
+  temperature = 0.1,              // randomness in predicting the output of the model                 
+  tools,                         // tools to be used in the model                       
 }: {
   messages: AIMessage[]
   temperature?: number
   model?: string
   tools?: { name: string; parameters: z.AnyZodObject }[]
 }) => {
-  const formattedTools = tools?.map((tool) => zodFunction(tool))
-  const response = await openai.chat.completions.create({
+  const formattedTools = tools?.map((tool) => zodFunction(tool))     // format the tools to be used in the model using the zod function                                                          
+  const response = await openai.chat.completions.create({            // call the openai api to get the completions of the model
     model,
     messages: [
-      {
+      {                                                             // combines the system prompt and the messages provided by the user                                                                                    
         role: 'system',
         content: systemPrompt,
       },
@@ -27,9 +28,9 @@ export const runLLM = async ({
     ],
     temperature,
     tools: formattedTools,
-    tool_choice: 'auto',
+    tool_choice: 'auto',                                                   // auto chooses the best tool to use in the model
     parallel_tool_calls: false,
   })
 
-  return response.choices[0].message
+  return response.choices[0].message                                          // return the first response of the model 
 }
